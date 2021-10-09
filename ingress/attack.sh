1b4f478bd93bc51e2fafa54e8476bbd7ff7cc8e6
imjeongwoo <limjeong1219@gmail.com>
2021-10-09 21:18:46 +0900

    vegeta: 5초 작업 초당 1개씩 60초 동안

--- ingress/attack.sh
+++ ingress/attack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 부하 테스팅
-vegeta --version && echo "GET http://127.0.0.1.xip.io/sleep/5" | vegeta attack -rate=1
+vegeta --version && echo "GET http://127.0.0.1.xip.io/sleep/5" | vegeta attack -rate=1 -keepalive=false -duration=60s | vegeta report
